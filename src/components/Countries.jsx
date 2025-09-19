01e37caf42992263b7f75d65c73b6cb07fb5592e
Masum Billah <mb6517640@gmail.com>
2025-09-19 07:21:06 +0600

    React app Setup

---
+++ src/components/Countries.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+
+            <h2>Hello world</h2>
+            
+        </div>
+    );
+};
+
+export default Countries;
